ac730fc496c6b47493c35d4197eb93ebac71bc9d
Joe <a.m.sadra4@gmail.com>
2026-09-25 22:05:01 +0330

    Update package.json and vite.config.ts for deployment configuration and homepage URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/reactReview/",
+  base: "/reactReview",
   plugins: [
     react(),
     tailwindcss(),
